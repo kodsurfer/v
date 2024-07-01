baa320005fef2381c845b011a41d0e57baed70c5
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-07-02 01:01:58 +0300

    update article.v

--- blog/article.v
+++ blog/article.v
@@ -11,3 +11,9 @@ pub fn (app &App) find_all_articles() []Article{
 		select from Article
 	} or { panic(err) }
 }
+
+pub fn (app &App) get_article() ?Article{
+	return sql app.db {
+		select from Article limit 1
+	} or { panic(err) } [0]
+}
