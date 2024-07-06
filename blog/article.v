35cc8dd47afe49545220c21597c21d2bb9ef2119
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-07-06 21:41:47 +0300

    Update article.v

--- blog/article.v
+++ blog/article.v
@@ -1,5 +1,7 @@
 module main
 
+import vweb
+
 struct Article {
 	id int @[primary; sql: serial]
 	title string
@@ -16,4 +18,10 @@ pub fn (app &App) get_article() ?Article{
 	return sql app.db {
 		select from Article limit 1
 	} or { panic(err) } [0]
+}
+
+@['/articles'; get]
+pub fn (mut app App) articles() vweb.Result {
+	articles := app.find_all_articles()
+	return app.json (articles)
 }
